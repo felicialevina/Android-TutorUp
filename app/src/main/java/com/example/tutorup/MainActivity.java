0e1c66c90b3860f486c258ee2cb55311e957b40a
apoorvaarya <apoorvaaryaat@gmail.com>
2020-11-07 01:44:01 -0500

    btnLogin

--- app/src/main/java/com/example/tutorup/MainActivity.java
+++ app/src/main/java/com/example/tutorup/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.btnLogin2);
+        Button button = (Button) findViewById(R.id.btnLogin);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
