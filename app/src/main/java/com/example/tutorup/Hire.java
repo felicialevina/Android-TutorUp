cff1964f3a79700867e05ae5a48180b928b252cd
Felicia Levina <felicialevina10@gmail.com>
2020-11-13 21:18:59 -0500

    Calendar Functionality Added

--- app/src/main/java/com/example/tutorup/Hire.java
+++ app/src/main/java/com/example/tutorup/Hire.java
@@ -3,11 +3,13 @@ package com.example.tutorup;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -16,6 +18,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class Hire extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -43,14 +48,29 @@ public class Hire extends AppCompatActivity {
 
         rating = ratingBar.getNumStars();
 
-        Button button = (Button) findViewById(R.id.btnHire);
+        final Calendar c = Calendar.getInstance();
+        final DateFormat frmtDate = DateFormat.getDateInstance();
+        final DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                c.set(Calendar.YEAR, year);
+                c.set(Calendar.MONTH, month);
+                c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            }
+        };
 
+        Button button = (Button) findViewById(R.id.btnHire);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                new DatePickerDialog(Hire.this, d, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        /*frmtDate.format(c.getTime())*/
+
+
+
+
     }
 }
